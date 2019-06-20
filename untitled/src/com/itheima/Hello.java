64fd925e2e0850b6011aeaf7ccac10eb5b93d246
xiaolaji <xioalaji@itcast.cn>
2019-06-20 16:38:09 +0800

    naskjldn

---
+++ untitled/src/com/itheima/Hello.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("那kid发错了");
+        System.out.println("那kid发错了");
+        System.out.println("那kid发错了");
+        System.out.println("那kid发错了");
+        System.out.println("那kid发错了");
+        System.out.println("那kid发错了");
+    }
+}
